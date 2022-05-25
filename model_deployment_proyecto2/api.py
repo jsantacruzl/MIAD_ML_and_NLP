8dc15b95f112a786e23d57ff398804dd22868320
jsantacruzl <92884059+jsantacruzl@users.noreply.github.com>
2022-05-25 14:32:35 -0500

    Primera Version Proyecto 2 del curso

---
+++ model_deployment_proyecto2/api.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python
+from flask import Flask
+from flask_restplus import Api, Resource, fields
+import joblib
+from m09_model_deployment import clasificar_pelicula
+
+
+# Definición aplicación Flask
+app = Flask(__name__)
+
+# Definición API Flask
+api = Api(
+    app, 
+    version='1.0', 
+    title='Movies Classifier API',
+    description='Movies Classifier API')
+
+ns = api.namespace('predict', 
+     description='Movies Classifier')
+
+# Definición argumentos o parámetros de la API
+parser = api.parser()
+parser.add_argument(
+    'Year', 
+    type=int, 
+    required=True, 
+    help='Movie''s Year', 
+    location='args')
+
+parser.add_argument(
+    'Title', 
+    type=str, 
+    required=True, 
+    help='Movie''s Title', 
+    location='args')
+
+parser.add_argument(
+    'Plot', 
+    type=str, 
+    required=True, 
+    help='Movie''s Plot', 
+    location='args')
+
+resource_fields = api.model('Resource', {
+    'result': fields.String,
+})
+
+
+@ns.route('/')
+class MovieApi(Resource):
+
+    @api.doc(parser=parser)
+    @api.marshal_with(resource_fields)
+    def get(self):
+        args = parser.parse_args()
+        
+        return {
+         "result": clasificar_pelicula(args['Year'], args['Title'], args['Plot'] )
+        }, 200
+    
+    
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=False, host='0.0.0.0', port=8888)
---
+++ model_deployment_proyecto2/m09_model_deployment.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python
+
+import pandas as pd
+import joblib
+import sys
+import os
+import numpy as np
+import nltk
+from nltk.stem import WordNetLemmatizer
+
+# Lematizacion
+wordnet_lemmatizer = WordNetLemmatizer()
+nltk.download('wordnet')
+def split_into_lemmas(text):
+    text = text.lower()
+    words = text.split()
+    return [wordnet_lemmatizer.lemmatize(word, pos='v') for word in words]
+
+def clasificar_pelicula(Year, Title, Plot):
+
+    clf = joblib.load(os.path.dirname(__file__) + '/clasificacion_peliculas.pkl') 
+    vect = joblib.load(os.path.dirname(__file__) + '/vectorizer.pkl')
+
+    data = [[Year, Title, Plot]]
+    df_params = pd.DataFrame(data, columns=['Year', 'Title', 'Plot'])
+    
+    X_test_dtm = vect.transform(df_params['Title']+' '+df_params['Plot'])
+    dfXTestTemp = pd.DataFrame(X_test_dtm.toarray(), index= df_params.index)
+    dfXTestFinal = df_params.join(dfXTestTemp)
+    dfXTestFinal = dfXTestFinal.drop('Title', 1)
+    dfXTestFinal = dfXTestFinal.drop('Plot', 1)
+    
+    y_pred_test_genres = clf.predict_proba(dfXTestFinal)
+    
+    cols = ['Action', 'Adventure', 'Animation', 'Biography', 'Comedy', 'Crime', 'Documentary', 'Drama', 'Family',
+        'Fantasy', 'Film-Noir', 'History', 'Horror', 'Music', 'Musical', 'Mystery', 'News', 'Romance',
+        'Sci-Fi', 'Short', 'Sport', 'Thriller', 'War', 'Western']
+    
+    res = pd.DataFrame(y_pred_test_genres, index=df_params.index, columns=cols)
+
+    res_final = pd.DataFrame(res.columns.values[np.argsort(-res.values, axis=1)[:, :3]], 
+                  index=res.index,
+                  columns = ['1st','2nd','3rd']).reset_index()
+    
+    res_final = res_final.drop('index', 1)
+    
+    return "\n".join(", ".join(map(str, xs)) for xs in res_final.itertuples(index=False))
+
+
+if __name__ == "__main__":
+    
+    if len(sys.argv) < 4:
+        print('Please add all parameters (Year, Title, Plot)')
+        
+    else:
+
+        y = sys.argv[1]
+        t = sys.argv[2]
+        p = sys.argv[3]
+
+
+        p1 = clasificar_pelicula(y,t,p)
+        
+        print('This movie can be classified as: ', p1)
+        
